ab0d7835078366236a88f5f36b42558812c1e0b7
akennerly <andrew@kennerly.net>
2020-09-03 21:23:38 -0400

    Set SEND_TO_ACURITE = False
    Since there is no longer any reason to send data to Acurite.  Set the default value the FALSE in the example config.

--- config.example.py
+++ config.example.py
@@ -3,4 +3,4 @@ STATION_ID = 'KCASANFR9999'
 STATION_KEY = 'xxxxxxx'
 FREQUENCY = 30 # number of seconds to send updates
 ACURITE_HUB_IP = '52.22.110.201' # the real ip for hubapi.myacurite.com
-SEND_TO_ACURITE = True # send data to acurite, while you still can
+SEND_TO_ACURITE = False # send data to acurite, while you still can
